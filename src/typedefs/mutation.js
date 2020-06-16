6aae5da519751a921c352523190c11195e81dbd8
Richard Dang <richardspacedang@gmail.com>
2020-06-16 15:14:58 -0400

    Fix apollo server dependency

--- src/typedefs/mutation.js
+++ src/typedefs/mutation.js
@@ -1,4 +1,5 @@
-const { gql } = require("apollo-server");
+const { gql } = require("apollo-server-express");
+
 
 exports.Mutation = gql`
   type Mutation {
--- src/typedefs/query.js
+++ src/typedefs/query.js
@@ -1,4 +1,5 @@
-const { gql } = require("apollo-server");
+const { gql } = require("apollo-server-express");
+
 
 exports.Query = gql`
   type Query {
